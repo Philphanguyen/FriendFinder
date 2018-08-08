896c6459698768586ca7236eda5a4b48ce0e35cf
Pip1242 <philphanguyen@gmail.com>
2018-08-08 14:37:36 -0400

    fixed?

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 //tells node we're creating express server
 var app = express();
 //sets initial port
-var PORT = process.env.port || 3000;
+const PORT = process.env.port || 3000;
 //sets up express app to handle data
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -12,6 +12,6 @@ app.use(bodyParser.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 //Listener, effectively starts server
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-})
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${ PORT }`);
+});
